f5c9ededf1dd5e3b9831c20232178ead70578709
nickl- <github@jigsoft.co.za>
2013-02-22 01:59:31 +0200

    Use setup teardown to play nice with PHPUnit

--- tests/library/Respect/Test/StreamWrapperTest.php
+++ tests/library/Respect/Test/StreamWrapperTest.php
@@ -1,11 +1,35 @@
 <?php
 namespace Respect\Test;
 
+use ReflectionObject, Exception, InvalidArgumentException;
 /**
  * @covers Respect\Test\StreamWrapper
  */
 class StreamWrapperTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var FileStreamEntity local instance
+     */
+    protected $object;
+
+    /**
+     * Setting up the stage before our unit tests run.
+     */
+    protected function setUp()
+    {
+        StreamWrapper::setStreamOverrides(array());
+        $this->object = new StreamWrapper;
+    }
+
+    /**
+     * Tearing thing down once test execution is done.
+     */
+    protected function tearDown()
+    {
+        StreamWrapper::releaseOverrides();
+        $this->object = null;
+    }
+
     /**
      * @covers Respect\Test\StreamWrapper::setStreamOverrides
      */
