13166dd818d0e08dc03bc05b8749bf1869b65712
nickl- <github@jigsoft.co.za>
2013-02-22 02:00:06 +0200

    Add more coverage tests

--- tests/library/Respect/Test/StreamWrapperTest.php
+++ tests/library/Respect/Test/StreamWrapperTest.php
@@ -45,8 +45,34 @@ class StreamWrapperTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * @covers Respect\Test\StreamWrapper::setStreamOverrides
+     */
+    public function testSetStreamOverrideResource()
+    {
+        StreamWrapper::setStreamOverrides(array(
+            'virtual/foo-bar-baz.ini' => fopen('data:text/plain;base64,'.base64_encode("foo=bar\nbaz=bat"), 'w+b'),
+            'virtual/happy-panda.ini' => fopen('data:text/plain;base64,'.base64_encode("panda=happy\nhappy=panda"), 'w+b'),
+        ));
+        $this->assertTrue(is_dir('virtual'));
+        $this->assertTrue(file_exists('virtual/happy-panda.ini'));
+        $this->assertEquals("foo=bar\nbaz=bat", file_get_contents('virtual/foo-bar-baz.ini'));
+    }
+
+    /**
+     * @covers Respect\Test\StreamWrapper::interfacePrep
+     */
+    public function testInterfacePrep()
+    {
+        $sw = new StreamWrapper;
+        Reflect::on($sw)->setProperty('methods', array());
+
+        StreamWrapper::setStreamOverrides(array());
+        $stat = $sw->url_stat('.', STREAM_URL_STAT_LINK);
+        $this->assertEquals('-1', $stat['blocks']);
+    }
+
+    /**
      * @covers Respect\Test\StreamWrapper::__call
-     * @depends testSetStreamOverrides
      * @expectedException Exception
      * @expectedExceptionMessage No method implemented for unknown
      */
@@ -57,8 +83,18 @@ class StreamWrapperTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * @covers Respect\Test\StreamWrapper::delegate
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage Invalid number of required arguments for method stream_open, expected 4 but found 0.
+     */
+    public function test__callInvalidArgs()
+    {
+        $sw = new StreamWrapper;
+        $sw->stream_open();
+    }
+
+    /**
      * @covers Respect\Test\StreamWrapper::releaseOverrides
-     * @depends test__call
      */
     public function testReleaseOverrides()
     {
@@ -69,12 +105,12 @@ class StreamWrapperTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @covers Respect\Test\StreamWrapper::__call
-     * @depends testReleaseOverrides
      * @expectedException Exception
      * @expectedExceptionMessage First inject stream overrides.
      */
     public function test__callInjectionRequired()
     {
+        StreamWrapper::releaseOverrides();
         $sw = new StreamWrapper;
         $sw->unknown();
     }
